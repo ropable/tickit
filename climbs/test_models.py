62bbd65deb32d385adc83533dfa1e72f429dbdc3
Ashley Felton <ashley@ropable.com>
2014-09-02 21:56:21 +0800

    More working tests.

--- climbs/test_models.py
+++ climbs/test_models.py
@@ -1,9 +1,12 @@
-#from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model
+from django.db import transaction
 from django.test.client import Client
 
 from django_nose import FastFixtureTestCase
 
-from .models import Business, Wall
+from .models import Business, Wall, Rope, Climb
+
+USER = get_user_model()
 
 
 class ClimbsTestCase(FastFixtureTestCase):
@@ -11,23 +14,41 @@ class ClimbsTestCase(FastFixtureTestCase):
     Defines fixtures and setup common to all model test cases.
     '''
     fixtures = [
-        #'test-user.json',
         'test-business.json',
         'test-wall.json',
+        'test-rope.json',
+        'test-climb.json',
     ]
     cleans_up_after_itself = True
 
+    @classmethod
+    def setUpClass(cls):
+        transaction.set_autocommit(True)
+        if USER.objects.filter(email='ashley@ropable.com').exists():
+            cls.user = USER.objects.filter(email='ashley@ropable.com')
+        else:
+            cls.user = USER.objects.create_user(
+                email='ashley@ropable.com',
+                password='test'
+            )
+        transaction.set_autocommit(False)
+
+    @classmethod
+    def tearDownClass(cls):
+        transaction.set_autocommit(True)
+        u = USER.objects.get(email='ashley@ropable.com')
+        u.delete()
+        transaction.set_autocommit(False)
+
     def setUp(self):
         self.client = Client()
-        self.client.login(email='ashley@ropable.com', password='test')
 
 
 class BusinessModelTest(ClimbsTestCase):
 
     def test_unicode(self):
         '''
-        Test model __unicode__() method.
-        Must return a unicode string.
+        Test model __unicode__() method. Must return a unicode string.
         '''
         for i in Business.objects.all():
             self.assertIsInstance(i.__unicode__(), unicode)
@@ -37,8 +58,27 @@ class WallModelTest(ClimbsTestCase):
 
     def test_unicode(self):
         '''
-        Test model __unicode__() method.
-        Must return a unicode string.
+        Test model __unicode__() method. Must return a unicode string.
         '''
         for i in Wall.objects.all():
+            self.assertIsInstance(i.__unicode__(), unicode)
+
+
+class RopeModelTest(ClimbsTestCase):
+
+    def test_unicode(self):
+        '''
+        Test model __unicode__() method. Must return a unicode string.
+        '''
+        for i in Rope.objects.all():
+            self.assertIsInstance(i.__unicode__(), unicode)
+
+
+class ClimbModelTest(ClimbsTestCase):
+
+    def test_unicode(self):
+        '''
+        Test model __unicode__() method. Must return a unicode string.
+        '''
+        for i in Climb.objects.all():
             self.assertIsInstance(i.__unicode__(), unicode)
--- climbs/test_views.py
+++ climbs/test_views.py
@@ -1,21 +1,19 @@
 from django.core.urlresolvers import reverse
-from django.test.client import Client
 
-#from .models import Business, Wall
+from .models import Business, Wall, Rope, Climb
 from .test_models import ClimbsTestCase as TestCase
 
 
 class ClimbDetailViewTest(TestCase):
 
-    def setUp(self):
-        self.client = Client()
-        url = reverse('climb_detail')
-        self.response = self.client.get(url)
-
     def test_url_names(self):
         # Test named URLs can be reversed.
-        self.assertIsNotNone(reverse('climb_detail'))
+        for i in Climb.objects.all():
+            self.assertIsNotNone(reverse('climb_detail', kwargs={'pk': i.pk}))
 
     def test_get(self):
         # Test response status and rendered context.
-        self.assertEqual(self.response.status_code, 200)
+        for i in Climb.objects.all():
+            url = reverse('climb_detail', kwargs={'pk': i.pk})
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
